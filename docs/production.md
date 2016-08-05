b2ba636288466f87dfd1e1eb11aa257331d38362
Hirosvk <kjnhiroyuki@hotmail.com>
2016-08-05 12:33:33 -0700

    writing specs

--- docs/production.md
+++ docs/production.md
@@ -1,18 +1,22 @@
-## 1. build backend up in develpoment
-* write Jasmine test for API endpoints
+## 1. build backend up in develpoment (1.5 day)
+* write Jasmine test for app's API endpoints
 * write Rails code
 * test w/o auth_token
 
-## 2. push to heroku / run the app in production
+## 2. push to heroku / run the app in production (0.5 day)
 * take care of gems, etc... before pushing to heroku
 * test w/ auth_token
 * get SSL key
 * set up SSL on heroku
 
-## 3. Slack command
+## 3. Slack Integration (0.5 day)
 * implement slack custom commands
 * test on channels
 
-## 4. extra feature
-* create a bot
+## 4. extra feature (1.5 day)
+* setup Interactive Buttons & Slack API
+* documentation
+
+## 5. more extra feature
 * add visual effects (emoji, pics, etc...)
+* create a bot
---
+++ test/controllers/ApiEndpointsSpec.js
@@ -0,0 +1,38 @@
+describe("Tic-Tac-Toe", function(){
+
+  describe("New Game", function(){
+    it("starts the game with opponent's username as parameter, and returns the empty board");
+    it("the starter can choose to go after the opponent");
+    it("returns instruction message with invalid entry");
+    it("returns error msg if the opponent's information is missing");
+    it("returns error msg if a game is alreayd taking place in the channel");
+    it("handles multiple games taking place in different channels");
+
+    // Interactive Button
+    it("askes if you want to abandon the previous game if there is a game in progress");
+
+    // API
+    it("returns error msg if the opponent is not logged in");
+  });
+
+  describe("Place Move", function(){
+    it("takes the coordinates and returns the updated board");
+    it("returns error msg to any moves other than the current player");
+  });
+
+  describe("game over", function(){
+    it("returns the result if the game has been won");
+    it("reutnrs the result if the game is a tie");
+  });
+
+  describe("reset game", function(){
+    it("the starter of the game can cancel the game");
+
+    // Interactive Button
+    it("to cancel the game, it requires the permission of the opponent");
+
+    // API & Event Listener
+    it("when the player logs out the game is automatically cancelled");
+  });
+
+})
