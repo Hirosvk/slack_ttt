24642bfd39675d9bef6dedb9535e840f67bbf963
Hirosvk <kjnhiroyuki@hotmail.com>
2016-08-07 23:01:16 -0700

    handles response_type

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -2,7 +2,7 @@ class Api::GamesController < ApplicationController
   after_filter :cors_set_access_control_headers
 
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Origin'] = '*slack.com'
     headers['Access-Control-Allow-Methods'] = 'POST, GET'
     headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type}.join(',')
   end
