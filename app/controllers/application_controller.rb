4addee1227260a54519018303050e1799a3d3570
Hirosvk <kjnhiroyuki@hotmail.com>
2016-08-07 18:47:53 -0700

    testing on Slack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, unless: :from_slack
-  
+
   def from_slack
-    request.params[:token] == "gIkuvaNzQIHg97ATvDxqgjtO"
+    request.params[:token] == "JMN4zMcOJJnJTpBiu4NNdtIr"
   end
+
+  SLACK_TOKENS = [
+    "JMN4zMcOJJnJTpBiu4NNdtIr", # /show_board
+    "gIkuvaNzQIHg97ATvDxqgjtO" # for testing
+  ]
 end
